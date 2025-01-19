6826661fd9888a1d749bae503574966070fcff9e
cacko <alex@cacko.net>
2025-01-19 05:45:02 +0000

    "update the diff to reflect changes to the new version of env.yml.index 02a795a..d21ebeb-1a.3.2.1.0.0e1.1e2.0"

--- masha/translate/__init__.py
+++ masha/translate/__init__.py
@@ -4,7 +4,6 @@ from fastapi import APIRouter, Request, HTTPException, Path as FPath
 import typer
 from pathlib import Path
 from typing_extensions import Annotated
-from transformers.models.marian.convert_marian_to_pytorch import convert as mconvert
 from transformers import MarianMTModel
 import torch
 
@@ -49,27 +48,3 @@ def translate(
     assert text
     t = Translator.translated(langs, " ".join(text))
     print(t)
-
-
-@cli.command()
-def convert(
-    model_path: Annotated[str, typer.Argument()],
-    dest_path: Annotated[str, typer.Option("-d", "--dest")],
-):
-    print(model_path, dest_path)
-    Path(dest_path).mkdir(parents=True, exist_ok=True)
-    mconvert(Path(model_path), Path(dest_path))
-
-    checkpoint = dest_path
-    model = MarianMTModel.from_pretrained(checkpoint)
-    state_dict = torch.load(checkpoint + "/pytorch_model.bin")
-
-    with torch.no_grad():
-        model.model.shared.weight[:] = state_dict["model.shared.weight"]
-
-    model.save_pretrained(
-        checkpoint, safe_serialization=False
-    )  # Overwrites the old checkpoint
-    model.save_pretrained(
-        checkpoint, safe_serialization=True
-    )  # Add safetensors while we're here
